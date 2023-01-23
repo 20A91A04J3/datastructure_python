ae7aa187519855c2930ab1feab7fc96d8aa14064
A_surya_kanthi <84856996+20A91A04J3@users.noreply.github.com>
2023-01-23 22:12:58 +0530

    Add files via upload

---
+++ oops2.py
@@ -0,0 +1,26 @@
+class personal:
+    num=123
+    def __init__(self,name,phno,adhar,email):
+        self.name=name
+        self.phno=phno
+        self.adhar=adhar
+        self.email=email
+    def display(self):
+        print(self.name,self.phno,self.adhar,self.email)
+    
+class student(personal):
+    
+    def __init__(self,name,phno,adhar,email,rollno,branch,college):
+        self.rollno=rollno
+        self.branch=branch
+        self.college=college
+        super().__init__(name,phno,adhar,email)
+    def display(self):
+        print(self.rollno,self.branch,self.college)
+        super().display()
+
+s1=student('surya',7785648902,234567890,'20a91a04j3@aec.edu.in',123,'ECE','Aditya')
+s1.display()
+print(s1.num)
+
+
